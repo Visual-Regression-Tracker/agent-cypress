f6bce764aa9d717f5e33ea85419f8d5af5c5fa5f
Pavel Strunkin <pashidlos@gmail.com>
2020-07-05 20:11:36 +0200

    examples removed

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,19 @@
+context('Visual Regression Tracker', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/viewport')
+  })
+
+  it('example', () => {
+    cy.get('#navbar').should('be.visible')
+
+    cy.track('Whole page')
+
+    cy.get('#navbar').track('Separate element')
+
+    cy.track("With additional options", {
+      os: "MacOS",
+      device: "Cloud agent",
+      diffTollerancePercent: 0,
+    })
+  })
+})
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,11 @@
-export * from './commands'
-export * from './vrt-plugin'
+declare namespace Cypress {
+    interface Chainable {
+        track(name: string, options?: Partial<Loggable & Timeoutable & ScreenshotOptions & TrackOptions>): Chainable<null>
+    }
 
-declare global {
-    namespace Cypress {
-        interface Chainable {
-            track(name: string, options?: Partial<Loggable & Timeoutable & ScreenshotOptions & TrackOptions>): Chainable<null>
-        }
-
-        interface TrackOptions {
-            os?: string;
-            device?: string;
-            diffTollerancePercent?: number;
-        }
+    interface TrackOptions {
+        os?: string;
+        device?: string;
+        diffTollerancePercent?: number;
     }
 }
