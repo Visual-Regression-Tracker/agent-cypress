355975253bdbb475f1adaeb39228394ae1c907ec
Pavel Strunkin <pashidlos@gmail.com>
2020-07-05 15:48:04 +0200

    export added

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,17 @@
+export * from './commands'
+export * from './vrt-plugin'
 
-/// <reference types="cypress" />
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            track(name: string, options?: Partial<Loggable & Timeoutable & ScreenshotOptions & TrackOptions>): Chainable<null>
+        }
 
-// typically custom commands are added in this support folder
-// so it makes sense to put their TypeScript definitions here
-// from the JavaScript specs loads this file using
-// the triple slash "reference" comment like this:
-//
-// /// <reference path="../support/index.d.ts" />
-
-declare namespace Cypress {
-    interface Chainable {
-        track(name: string, options?: Partial<Loggable & Timeoutable & ScreenshotOptions & TrackOptions>): Chainable<null>
-    }
-
-    interface TrackOptions {
-        os?: string;
-        viewport?: string;
-        device?: string;
-        diffTollerancePercent?: number;
+        interface TrackOptions {
+            os?: string;
+            viewport?: string;
+            device?: string;
+            diffTollerancePercent?: number;
+        }
     }
 }
