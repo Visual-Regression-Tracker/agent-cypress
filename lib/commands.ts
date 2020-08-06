9101181f90fd2645654fc7f6116bcd08cfffe49d
Pavel Strunkin <pashidlos@gmail.com>
2020-08-06 19:05:59 +0200

    refactoring

--- lib/commands.ts
+++ lib/commands.ts
@@ -7,36 +7,39 @@ export const addTrackCommand = () =>
       prevSubject: ["optional", "element", "window", "document"],
     },
     (subject, name, options) => {
-
       let imagePath: string;
       let pixelRatio: number;
       const target = subject ? cy.wrap(subject) : cy;
 
-      target.screenshot(name, {
-        ...options,
-        onAfterScreenshot: (el, props) => {
-          imagePath = props.path;
-          pixelRatio = props.pixelRatio;
-          return options?.onAfterScreenshot;
-        },
-      })
-        .then(() => cy.task("ENCODE_IMAGE", { imagePath }, { log: false }))
-        .then((imageBase64) => cy.task("TRACK_IMAGE",
-          {
-            name,
-            imageBase64,
-            browser: Cypress.browser.name,
-            pixelRatio,
-            os: options?.os,
-            device: options?.device,
-            diffTollerancePercent: options?.diffTollerancePercent,
+      target
+        .screenshot(name, {
+          ...options,
+          onAfterScreenshot: (el, props) => {
+            imagePath = props.path;
+            pixelRatio = props.pixelRatio;
+            return options?.onAfterScreenshot;
           },
-          { log: false })
-        )
-        .then(err => {
-          if (err) {
-            throw new Error(err)
-          }
         })
+        .then(() => cy.task("ENCODE_IMAGE", { imagePath }, { log: false }))
+        .then((imageBase64) =>
+          cy.task(
+            "TRACK_IMAGE",
+            {
+              name,
+              imageBase64,
+              browser: Cypress.browser.name,
+              pixelRatio,
+              os: options?.os,
+              device: options?.device,
+              diffTollerancePercent: options?.diffTollerancePercent,
+            },
+            { log: false }
+          )
+        )
+        .then((err) => {
+          if (err) {
+            throw new Error(err);
+          }
+        });
     }
   );
