39da7aab72decf4c1130e7cdac9b3d50226af62d
Pavel Strunkin <pashidlos@gmail.com>
2020-09-14 16:21:47 +0200

    Do not start build if was started before
    https://github.com/Visual-Regression-Tracker/agent-cypress/issues/28

--- cypress/integration/example2.spec.js
+++ cypress/integration/example2.spec.js
@@ -1,18 +1,18 @@
 /* global cy */
 
-context("Visual Regression Tracker", () => {
+context("Visual Regression Tracker2", () => {
   beforeEach(() => {
     cy.visit("/commands/viewport");
   });
 
-  it("example", () => {
+  it("example2", () => {
     cy.get("#navbar").should("be.visible");
 
-    cy.vrtTrack("Whole page");
+    cy.vrtTrack("Whole page2");
 
-    cy.get("#navbar").vrtTrack("Separate element");
+    cy.get("#navbar").vrtTrack("Separate element2");
 
-    cy.vrtTrack("With additional options", {
+    cy.vrtTrack("With additional options2", {
       os: "MacOS",
       device: "Cloud agent",
       diffTollerancePercent: 0,
